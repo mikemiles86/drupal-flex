585d9b04f0392457c650d9bd3b66481910bc1691
Mike Miles <MMiles@genuineinteractive.com>
2016-06-22 00:00:16 -0400

    update with new base

---
+++ plugin/set-aside/set-aside.js
@@ -0,0 +1,39 @@
+Reveal.addEventListener('ready', function(event){
+  var print = window.location.search.match( /print-pdf/gi );
+  var set_aside = document.getElementsByClassName("slides")[0].hasAttribute("data-captions-aside");
+  var remove_aside = document.getElementsByClassName("slides")[0].hasAttribute("data-remove-aside");
+  var move_aside = document.getElementsByClassName("slides")[0].hasAttribute("data-move-aside");
+
+  if (!print && set_aside) {
+    var caption = false;
+    while (caption = document.getElementsByClassName("caption")[0]) {
+      var text = document.createTextNode(caption.textContent);
+      var aside = document.createElement('aside');
+      aside.appendChild(text);
+      aside.className += ' notes';
+      caption.parentNode.replaceChild(aside, caption);
+    }
+  }
+
+  if (print) {
+    var skips = document.querySelectorAll('[data-skip-print]');
+    var s = 0;
+    for (s = 0; s < skips.length; s++) {
+      skips[s].parentNode.removeChild(skips[s]);
+    }
+  }
+
+  if (remove_aside) {
+    while (aside = document.getElementsByClassName("notes")[0]) {
+      aside.parentNode.removeChild(aside);
+    }
+  }
+
+  if (move_aside) {
+    var moves = document.querySelectorAll('[data-set-aside]');
+    var m = 0;
+    for (m = 0; m < moves.length; m++) {
+      moves[m].parentNode.removeChild(moves[m]);
+    }
+  }
+});
